17881ed080173d7898f1b6914a3a5336d1f2fe20
vladyslav <vlad.bes0711@gmail.com>
2020-05-21 17:53:21 +0300

    implemented movie session and cinema hall daos

--- src/main/java/com/dev/cinema/Main.java
+++ src/main/java/com/dev/cinema/Main.java
@@ -1,8 +1,15 @@
 package com.dev.cinema;
 
 import com.dev.cinema.lib.Injector;
+import com.dev.cinema.model.CinemaHall;
 import com.dev.cinema.model.Movie;
+import com.dev.cinema.model.MovieSession;
+import com.dev.cinema.service.CinemaHallService;
 import com.dev.cinema.service.MovieService;
+import com.dev.cinema.service.MovieSessionService;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Main {
     public static final Injector INJECTOR = Injector.getInstance("com.dev.cinema");
@@ -15,6 +22,23 @@ public class Main {
         movieService.add(movie);
 
         movieService.getAll().forEach(System.out::println);
+
+        CinemaHallService cinemaHallService = (CinemaHallService) INJECTOR.getInstance(CinemaHallService.class);
+        cinemaHallService.getAll().forEach(System.out::println);
+        CinemaHall cinemaHall = new CinemaHall();
+        cinemaHall.setCapacity(50);
+        cinemaHall.setDescription("Cinema hall - A");
+        cinemaHallService.add(cinemaHall);
+        cinemaHallService.getAll().forEach(System.out::println);
+
+        MovieSessionService movieSessionService = (MovieSessionService) INJECTOR.getInstance(MovieSessionService.class);
+        MovieSession movieSession = new MovieSession();
+        movieSession.setCinemaHall(cinemaHall);
+        movieSession.setMovie(movie);
+        movieSession.setShowTime(LocalDateTime.now().plusDays(1));
+        movieSessionService.add(movieSession);
+        movieSessionService.findAvailableSessions(movie.getId(), LocalDate.now()).forEach(System.out::println);
+
     }
 
 }
--- src/main/java/com/dev/cinema/dao/impl/CinemaHallDaoImpl.java
+++ src/main/java/com/dev/cinema/dao/impl/CinemaHallDaoImpl.java
@@ -1,4 +1,60 @@
 package com.dev.cinema.dao.impl;
 
-public class CinemaHallDao {
+import com.dev.cinema.dao.CinemaHallDao;
+import com.dev.cinema.lib.Dao;
+import com.dev.cinema.model.CinemaHall;
+import com.dev.cinema.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+@Dao
+public class CinemaHallDaoImpl implements CinemaHallDao {
+    @Override
+    public CinemaHall add(CinemaHall cinemaHall) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            Long cinemaHallId = (Long) session.save(cinemaHall);
+            transaction.commit();
+            cinemaHall.setId(cinemaHallId);
+            return cinemaHall;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't add CinemaHall entity", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<CinemaHall> getAll() {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<CinemaHall> cr = cb.createQuery(CinemaHall.class);
+            Root<CinemaHall> root = cr.from(CinemaHall.class);
+            cr.select(root);
+            Query<CinemaHall> query = session.createQuery(cr);
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get all CinemaHalls entityes", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
 }
