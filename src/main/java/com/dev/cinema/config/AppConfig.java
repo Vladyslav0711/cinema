24f0790dece0a40d6a38b987d72d76455878eb79
vladyslav <vlad.bes0711@gmail.com>
2020-06-10 12:38:30 +0300

    use setPackagesToScan

--- src/main/java/com/dev/cinema/config/AppConfig.java
+++ src/main/java/com/dev/cinema/config/AppConfig.java
@@ -1,12 +1,5 @@
 package com.dev.cinema.config;
 
-import com.dev.cinema.model.CinemaHall;
-import com.dev.cinema.model.Movie;
-import com.dev.cinema.model.MovieSession;
-import com.dev.cinema.model.Order;
-import com.dev.cinema.model.ShoppingCart;
-import com.dev.cinema.model.Ticket;
-import com.dev.cinema.model.User;
 import java.util.Properties;
 import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
@@ -51,9 +44,6 @@ public class AppConfig {
         properties.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
         factoryBean.setHibernateProperties(properties);
         factoryBean.setPackagesToScan("com.dev.cinema.model");
-        factoryBean.setAnnotatedClasses(CinemaHall.class,
-                Movie.class, MovieSession.class, Order.class, ShoppingCart.class,
-                Ticket.class, User.class);
         return factoryBean;
     }
 }
